8036909a8a04099e6002971d235b7d41f9d1a325
myakisheff <n.s.myakishev@yandex.ru>
2023-08-16 19:39:08 +0300

    added shapes to game activity with random colors

--- app/src/main/java/com/example/figurefind/MainActivity.kt
+++ app/src/main/java/com/example/figurefind/MainActivity.kt
@@ -1,11 +1,30 @@
  package com.example.figurefind
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
+
+
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
+
+        val viewGroup = findViewById<GridLayout>(R.id.gridLayout)
+        viewGroup.forEach {
+            if(it is ImageButton){
+                it.setColorFilter(setRandomColor())
+            }
+        }
     }
+
+     private fun setRandomColor(): Int{
+         val range = 0..255
+         return Color.rgb(range.random(), range.random(), range.random())
+     }
+
 }
