323a2560bda877c227335ba38b2bd6862ec2685b
myakisheff <n.s.myakishev@yandex.ru>
2023-08-16 21:48:57 +0300

    added shapes

---
+++ app/src/main/java/com/example/figurefind/CompositeShape.kt
@@ -0,0 +1,14 @@
+package com.example.figurefind
+
+class Shape(
+    val countShapes: Int,
+) {
+    fun chooseShape(){
+        when(countShapes)
+    }
+}
+
+enum class CompositeShapes{
+    SQUARE_CIRCLE,
+    
+}
