4ef2628cd960f5f08127bce858621823ebfe727a
myakisheff <n.s.myakishev@yandex.ru>
2023-08-16 19:58:03 +0300

    added main shape to solve

--- app/src/main/java/com/example/figurefind/MainActivity.kt
+++ app/src/main/java/com/example/figurefind/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.widget.GridLayout
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.forEach
 
@@ -13,6 +14,14 @@ import androidx.core.view.forEach
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        //TODO: creates a drawable file and show it in ImageView with id shapeToFind
+
+        val shapeToFind = findViewById<ImageView>(R.id.shapeToFind)
+        shapeToFind.setImageResource(R.drawable.ic_shape_default)
+
+        val darkColor = Color.rgb(160, 160, 160)
+        shapeToFind.setColorFilter(darkColor)
 
         val viewGroup = findViewById<GridLayout>(R.id.gridLayout)
         viewGroup.forEach {
